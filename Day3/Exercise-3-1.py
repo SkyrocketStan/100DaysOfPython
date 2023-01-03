2fa2aadfb06a53501a37a8149476295509b9e446
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-03 14:21:17 +0300

    Day 3 / Exercise 1 - Odd or Even

---
+++ Day3/Exercise-3-1.py
@@ -0,0 +1,14 @@
+# Write a program that works out whether if a given number is an odd or even number.
+
+
+
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if number % 2 == 0 :
+    print("This is an even number.")
+else:
+    print("This is an odd number.")
