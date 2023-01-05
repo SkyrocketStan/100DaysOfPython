2c75dd2b2eabf05342dec8d92e6700f2d3fdac7f
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-05 11:52:28 +0300

    Day 5 / Exercise 2 - High Score

--- Day5/Exercise-5-2.py
+++ Day5/Exercise-5-2.py
@@ -2,7 +2,7 @@
 #
 # e.g. student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
 #
-# Important you are not allowed to use the max or min functions. The output words must match the example. i.e
+# Important you are not allowed to use the max or min functions. The output words must match the example. i.e.
 #
 # The highest score in the class is: x
 
