bdd841dd189c925891c5b1e41b8cb5b857482c17
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-07 12:32:10 +0300

    Day-7-Hangman-4-end

--- Day7/Day-7-Hangman-4.py
+++ Day7/Day-7-Hangman-4.py
@@ -66,6 +66,7 @@ word_length = len(chosen_word)
 
 # TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
 # Set 'lives' to equal 6.
+lives = 6
 
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -88,7 +89,11 @@ while not end_of_game:
     # TODO-2: - If guess is not a letter in the chosen_word,
     # Then reduce 'lives' by 1.
     # If lives goes down to 0 then the game should stop and it should print "You lose."
-
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            end_of_game = True
+            print("You lose.")
     # Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
@@ -99,3 +104,4 @@ while not end_of_game:
 
     # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has
     #  remaining.
+    print(stages[lives])
