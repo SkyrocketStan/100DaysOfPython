3da7f5cbeb194670cd4954ad145c4332f687a098
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-07 11:24:39 +0300

    Day-7-Hangman-1-Start

---
+++ Day7/Day-7-Hangman.py
@@ -0,0 +1,10 @@
+# Step 1
+
+word_list = ["aardvark", "baboon", "camel"]
+
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
