937ff46d31119b4b465013cfc966837043de2a5a
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-09 14:23:15 +0300

    day-9-start

---
+++ Day9/day-9.py
@@ -0,0 +1,3 @@
+programming_dictionary = {
+    "Bug": "An error in a program that prevents the program from running as expected.",
+    "Function": "A piece of code that you can easily call over and over again."}
