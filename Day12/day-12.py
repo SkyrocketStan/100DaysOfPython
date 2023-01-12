f4a558cb041481991f6ed595c2bb88a174d355c5
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-12 15:17:10 +0300

    day-12-start

---
+++ Day12/day-12.py
@@ -0,0 +1,12 @@
+################### Scope ####################
+
+enemies = 1
+
+
+def increase_enemies():
+    enemies = 2
+    print(f"enemies inside function: {enemies}")
+
+
+increase_enemies()
+print(f"enemies outside function: {enemies}")
