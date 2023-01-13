2d8fcbf33acc629dccf50c37cbfaf53cd076568c
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-13 10:49:24 +0300

    day-13 Use a Debugger

--- Day 13/day-13.py
+++ Day 13/day-13.py
@@ -36,12 +36,14 @@ word_per_page = int(input("Number of words per page: "))
 total_words = pages * word_per_page
 print(total_words)
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
 
-# mutate([1,2,3,5,8,13])
+# Use a Debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1, 2, 3, 5, 8, 13])
