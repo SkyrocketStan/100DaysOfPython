e28cda778d58d5ac352249cbfe89edf035f07b99
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-13 10:00:43 +0300

    day-13 my_function()

--- Day 13/day-13.py
+++ Day 13/day-13.py
@@ -1,11 +1,13 @@
-############DEBUGGING#####################
+# ###########DEBUGGING#####################
 
-# # Describe Problem
-# def my_function():
-#   for i in range(1, 20):
-#     if i == 20:
-#       print("You got it")
-# my_function()
+# Describe Problem
+def my_function():
+    for i in range(1, 21):
+        if i == 20:
+            print("You got it")
+
+
+my_function()
 
 # # Reproduce the Bug
 # from random import randint
