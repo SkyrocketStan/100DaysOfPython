80c618365aa94ce7bf5037dd64e3802ce2e55193
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-13 10:04:20 +0300

    day-13 Reproduce the Bug

--- Day 13/day-13.py
+++ Day 13/day-13.py
@@ -10,10 +10,11 @@ def my_function():
 my_function()
 
 # # Reproduce the Bug
-# from random import randint
-# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
-# print(dice_imgs[dice_num])
+from random import randint
+
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(0, 5)
+print(dice_imgs[dice_num])
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
