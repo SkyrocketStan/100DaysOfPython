d797b642575a8ca2607a2212de8a00d16f8e3a67
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-13 10:09:47 +0300

    day-13 Play Computer

--- Day 13/day-13.py
+++ Day 13/day-13.py
@@ -16,12 +16,12 @@ dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
 dice_num = randint(0, 5)
 print(dice_imgs[dice_num])
 
-# # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year <= 1996:
+    print("You are a millenial.")
+elif year > 1996:
+    print("You are a Gen Z.")
 
 # # Fix the Errors
 # age = input("How old are you?")
