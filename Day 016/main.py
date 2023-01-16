101b39e58376d20059d10e9522deac3a404b38a8
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-16 12:27:27 +0300

    Day 16: turtle

---
+++ Day 016/main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+my_screen = Screen()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+print(my_screen.canvwidth)
+my_screen.exitonclick()
