ded9995478a6fa6448673b263b701f3b4b4992c5
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-18 13:21:59 +0300

    day-18-4-end

--- Day 018/day-18-4.py
+++ Day 018/day-18-4.py
@@ -1,3 +1,4 @@
+import random
 import turtle as t
 
 tim = t.Turtle()
@@ -5,3 +6,14 @@ tim = t.Turtle()
 ########### Challenge 4 - Random Walk ########
 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
            "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+screen = t.Screen()
+tim.speed(0)
+tim.pensize(15)
+
+for _ in range(50):
+    tim.color(random.choice(colours))
+    tim.fd(50)
+    tim.setheading(random.choice([0, 90, 180, 270]))
+
+screen.exitonclick()
