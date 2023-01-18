86671f4c37be39dda17cc8017bc908b47cecdac2
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-18 11:40:54 +0300

    day-18-1-end

--- Day 018/day-18-1.py
+++ Day 018/day-18-1.py
@@ -1,14 +1,23 @@
-#####Turtle Intro######
+# ####Turtle Intro######
 
 import turtle as t
 
 timmy_the_turtle = t.Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.backward(200)
-timmy_the_turtle.right(90)
-timmy_the_turtle.left(180)
-timmy_the_turtle.setheading(0)
 
-######## Challenge 1 - Draw a Square ############
+
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.backward(200)
+# timmy_the_turtle.right(90)
+# timmy_the_turtle.left(180)
+# timmy_the_turtle.setheading(0)
+
+# ####### Challenge 1 - Draw a Square ############
+def l_turn():
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+
+
+for _ in range(4):
+    l_turn()
