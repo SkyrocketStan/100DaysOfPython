4fbd76da545f0b40599db4a3ef798d65b1f25ea2
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-18 11:34:53 +0300

    day-18-1-start

---
+++ Day 018/day-18-1.py
@@ -0,0 +1,14 @@
+#####Turtle Intro######
+
+import turtle as t
+
+timmy_the_turtle = t.Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.backward(200)
+timmy_the_turtle.right(90)
+timmy_the_turtle.left(180)
+timmy_the_turtle.setheading(0)
+
+######## Challenge 1 - Draw a Square ############
