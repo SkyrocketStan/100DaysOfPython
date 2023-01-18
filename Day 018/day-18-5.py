e5a0085dd9cd7c128a8d34ebd7cfd1642bb1c25f
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-18 13:36:44 +0300

    day-18-5-start

---
+++ Day 018/day-18-5.py
@@ -0,0 +1,15 @@
+import random
+import turtle as t
+
+tim = t.Turtle()
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+########### Challenge 5 - Spirograph ########
