6a750d1ed9c8de0e602ea7c7d069520fa7db5169
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-18 12:54:03 +0300

    day-18-3-end

--- Day 018/day-18-3.py
+++ Day 018/day-18-3.py
@@ -1,5 +1,16 @@
 import turtle as t
+from random import randint
 
 tim = t.Turtle()
+t.colormode(255)
 
 ########### Challenge 3 - Draw Shapes ########
+
+for angles in range(4, 9):
+    angle = 360 / angles
+    print(angle)
+    tim.color(randint(0, 255), randint(0, 255), randint(0, 255))
+
+    for _ in range(angles):
+        tim.fd(100)
+        tim.right(angle)
