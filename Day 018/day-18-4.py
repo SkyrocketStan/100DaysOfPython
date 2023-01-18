06bb7a0c6d0aa5a9441ac0f4df9c84dcce4150ff
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-18 12:55:20 +0300

    day-18-4-start

---
+++ Day 018/day-18-4.py
@@ -0,0 +1,7 @@
+import turtle as t
+
+tim = t.Turtle()
+
+########### Challenge 4 - Random Walk ########
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue",
+           "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
