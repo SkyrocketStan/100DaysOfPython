0f2ea21d7df76ec9210674947fbbc3fa347c0a10
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-19 17:00:18 +0300

    Day 19: Turtle race

--- Day 019/turtle-race.py
+++ Day 019/turtle-race.py
@@ -1,6 +1,41 @@
+import random
 from turtle import Turtle, Screen
 
-tim = Turtle()
+in_game = False
+
 screen = Screen()
+screen.title("Turtle race")
+screen.setup(width=500, height=400)
+end_line = screen.screensize()[0] / 2 + 20
+
+bet_color = screen.textinput(title="Make your bet",
+                             prompt="which turtle will win the race? Enter a "
+                                    "color: ")
+
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+turtles = []
+y = -120
+for color in colors:
+    t = Turtle(shape="turtle")
+    t.color(color)
+    turtles.append(t)
+    t.penup()
+    t.goto(x=-230, y=y)
+    y += 50
+
+if bet_color:
+    in_game = True
+win_color = None
+while in_game:
+
+    for t in turtles:
+        dist = random.randint(1, 10)
+        t.fd(dist)
+        if t.xcor() >= end_line:
+            in_game = False
+            win_color = t.pencolor()
+
+win_or_not = "won" if bet_color.lower() == win_color else "lost"
+print(f"You've {win_or_not}! The {win_color} turtle is the winner! ")
 
 screen.exitonclick()
