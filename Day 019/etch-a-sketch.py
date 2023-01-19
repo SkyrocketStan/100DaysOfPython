6e7c83799390cb057ced655694cceed4092b39f8
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-19 14:08:26 +0300

    Day 19: Etch-A-Sketch

---
+++ Day 019/etch-a-sketch.py
@@ -0,0 +1,38 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def turn_counter_clockwise():
+    tim.left(10)
+
+
+def turn_clockwise():
+    tim.right(10)
+
+
+def clear_screen():
+    tim.clear()
+    tim.pu()
+    tim.home()
+    tim.pd()
+
+
+screen.listen()
+screen.title("Etch-A-Sketch")
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=turn_counter_clockwise)
+screen.onkey(key="d", fun=turn_clockwise)
+screen.onkey(key="c", fun=clear_screen)
+
+screen.exitonclick()
