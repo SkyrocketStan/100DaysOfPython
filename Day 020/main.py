dc36428ae8ccdf0d9f783fd79929d5557f0a9f91
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-20 17:41:10 +0300

    Day 20: Snake. OOP.

--- Day 020/main.py
+++ Day 020/main.py
@@ -1,5 +1,7 @@
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
+
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,33 +9,11 @@ screen.title("Snake game")
 screen.bgcolor("black")
 screen.tracer(0)
 
-snake_body = []
-
-
-def make_snake_body_init(body: list) -> list:
-    pos_x = 0
-    for _ in range(3):
-        t = Turtle("square")
-        t.color("white")
-        t.penup()
-        t.setx(pos_x)
-        body.append(t)
-        pos_x -= 20
-
-    return body
-
-
-snake_body: list = make_snake_body_init(snake_body)
-
 game_over = False
+snake = Snake()
 while not game_over:
     screen.update()
     time.sleep(0.1)
-
-    for i in range(len(snake_body) - 1, 0, -1):
-        cords = snake_body[i - 1].pos()
-        # print(f"i = {i}, pos = {cords}")
-        snake_body[i].setpos(cords)
-    snake_body[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
