b80a912a4116679094bd646e16b9c0c07ad94a18
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-20 17:42:11 +0300

    Day 20: Snake. Tidy up

--- Day 020/snake.py
+++ Day 020/snake.py
@@ -13,30 +13,9 @@ class Snake:
             self.body.append(t)
             pos_x -= 20
 
-    # self.snake_body: list = self.make_snake_body_init()
-
-    # def make_snake_body_init(self) -> list:
-    #     pos_x = 0
-    #     body = []
-    #     for _ in range(3):
-    #         t = Turtle("square")
-    #         t.color("white")
-    #         t.penup()
-    #         t.setx(pos_x)
-    #         body.append(t)
-    #         pos_x -= 20
-    #
-    #     return body
-
     def move(self):
         for i in range(len(self.body) - 1, 0, -1):
             cords = self.body[i - 1].pos()
             # print(f"i = {i}, pos = {cords}")
             self.body[i].setpos(cords)
         self.body[0].forward(20)
-    # def move(self):
-    #     for i in range(len(self.snake_body) - 1, 0, -1):
-    #         cords = self.snake_body[i - 1].pos()
-    #         # print(f"i = {i}, pos = {cords}")
-    #         self.snake_body[i].setpos(cords)
-    #     self.snake_body[0].forward(20)
