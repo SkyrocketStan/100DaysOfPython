8dddd017751385ecf194fed66a6dc5d12fae058f
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-22 18:23:22 +0300

    Day 22: Pong. Right paddle

--- Day 022/pong_game.py
+++ Day 022/pong_game.py
@@ -1,4 +1,7 @@
+import time
 from turtle import Screen
+
+from paddle import Paddle
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -7,5 +10,25 @@ screen = Screen()
 screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
 screen.title("PONG game")
 screen.bgcolor("black")
+screen.tracer(0)
+
+right_paddle = Paddle(350, 0)
+
+on_game = True
+
+
+def quit_game():
+    global on_game
+    on_game = False
+
+
+screen.listen()
+screen.onkey(right_paddle.up, "Up")
+screen.onkey(right_paddle.down, "Down")
+screen.onkey(quit_game, "q")
+
+while on_game:
+    screen.update()
+    time.sleep(0.001)
 
 screen.exitonclick()
