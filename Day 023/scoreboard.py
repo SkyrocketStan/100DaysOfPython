3d9d68cdcfef227577ddcaef6dc038a136ec7f1e
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-23 18:00:26 +0300

    Day 23: Turtle race. Scoreboard

--- Day 023/scoreboard.py
+++ Day 023/scoreboard.py
@@ -1,5 +1,29 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
+TEXT_ALIGN = "center"
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self, x_pos=-200, y_pos=250):
+        super().__init__()
+        self.score = 1
+        self.color("black")
+        self.setpos(x_pos, y_pos)
+        self.penup()
+        self.speed(0)
+        self.hideturtle()
+        self.update_text()
+
+    def update_score(self):
+        self.score += 1
+        self.update_text()
+
+    def update_text(self):
+        self.clear()
+        self.write(f"Level: {self.score}", move=False, align=TEXT_ALIGN,
+                   font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", move=False, align=TEXT_ALIGN, font=FONT)
