d66eed2f1b5c6c6e066d5b8e129bb36e9b9c67ab
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-23 21:38:54 +0300

    Day 23: Turtle race. Play with cars

--- Day 023/car_manager.py
+++ Day 023/car_manager.py
@@ -1,7 +1,54 @@
+import random
+from turtle import Turtle
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self, width, height):
+
+        super().__init__()
+        self.hideturtle()
+        self.cars = []
+        self.screen_width = width
+        self.screen_height = height
+        self.move_distance = STARTING_MOVE_DISTANCE
+        self.right_edge = (self.screen_width // 2)
+        self.left_edge = self.right_edge * -1
+
+    def generate_car(self):
+        random_chance = random.randint(1, 6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            # new_car.move_distance = STARTING_MOVE_DISTANCE
+            # new_car.right_edge = (self.width // 2)
+            # new_car.left_edge = self.right_edge * -1
+            # new_car.height = self.height
+            self.go_random_start_pos(new_car)
+            self.cars.append(new_car)
+
+    def go_random_start_pos(self, car):
+        # self.color(random.choice(COLORS))
+        car.goto(self.random_pos())
+
+    def random_pos(self):
+        high_y = (self.screen_height // 2) - 50
+        low_y = high_y * -1
+        y_pos = random.randint(low_y, high_y)
+        x_pos = self.right_edge - 20
+        return x_pos, y_pos
+
+    def move_cars(self):
+        for car in self.cars:
+            if car.xcor() <= self.left_edge:
+                self.go_random_start_pos(car)
+            else:
+                car.backward(self.move_distance)
+
+    def speed_up(self):
+        self.move_distance += MOVE_INCREMENT
--- Day 023/main.py
+++ Day 023/main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 
+from car_manager import CarManager
 from player import Player
 from scoreboard import Scoreboard
 
@@ -9,6 +10,7 @@ SCREEN_HEIGHT = 600
 screen = Screen()
 screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.tracer(0)
+screen.title("Turtle race")
 
 player = Player()
 scoreboard = Scoreboard()
@@ -24,14 +26,22 @@ screen.listen()
 screen.onkey(player.up, "Up")
 screen.onkey(quit_game, "q")
 
+car_manager = CarManager(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
+
 sleep_timer = 0.1
 while game_is_on:
     time.sleep(sleep_timer)
     screen.update()
+
+    car_manager.generate_car()
+    car_manager.move_cars()
     if player.is_finished():
         scoreboard.update_score()
         player.start_over()
-        sleep_timer *= 0.9
+        car_manager.speed_up()
+    for car in car_manager.cars:
+        if car.distance(player) < 20:
+            quit_game()
 
 scoreboard.game_over()
 screen.exitonclick()
