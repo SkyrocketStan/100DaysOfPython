e974b556e7d5dd5ba580806b568ca1930c99bb5b
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-23 18:00:36 +0300

    Day 23: Turtle race. Player

--- Day 023/player.py
+++ Day 023/player.py
@@ -1,7 +1,24 @@
+from turtle import Turtle
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
+WIDTH = 20
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__("turtle")
+        self.color("black")
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.goto(self.xcor(), self.ycor() + MOVE_DISTANCE)
+
+    def is_finished(self):
+        return True if self.ycor() >= FINISH_LINE_Y else False
+
+    def start_over(self):
+        self.goto(STARTING_POSITION)
