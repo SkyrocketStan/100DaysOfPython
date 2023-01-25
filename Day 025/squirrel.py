3bcc28856567dd5f3cae22d6dd03fa204fcf70ed
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-25 20:19:04 +0300

    Day 25: Squirrel counter

--- Day 025/squirrel.py
+++ Day 025/squirrel.py
@@ -1,5 +1,14 @@
 import pandas
 
 data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+grey_sq = len(data[data["Primary Fur Color"] == "Gray"])
+cinnamon_sq = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black_sq = len(data[data["Primary Fur Color"] == "Black"])
 
-# Fur color, Count
+data_dict = {
+    "Fur Color": ["Gray", "Cinnamon", "Black"],
+    "Count": [grey_sq, cinnamon_sq, black_sq]
+}
+
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
