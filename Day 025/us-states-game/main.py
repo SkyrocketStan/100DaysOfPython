033093c1fe074a5f6ceba2e5901b00828d908bde
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-26 15:07:22 +0300

    Day 26 / Day 25 states game refactor

--- Day 025/us-states-game/main.py
+++ Day 025/us-states-game/main.py
@@ -1,4 +1,5 @@
 import turtle
+
 import pandas
 
 screen = turtle.Screen()
@@ -12,13 +13,12 @@ all_states = data.state.to_list()
 guessed_states = []
 
 while len(guessed_states) < 50:
-    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
-                                    prompt="What's another state's name?").title()
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/50 States Correct",
+        prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in guessed_states:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in
+                          guessed_states]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
