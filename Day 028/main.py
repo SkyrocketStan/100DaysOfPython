e4d43e9569bbbd4ba96a4ac1fa248cef870407d6
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-28 14:33:25 +0300

    Day 28: pomodoro refactor

--- Day 028/main.py
+++ Day 028/main.py
@@ -1,7 +1,7 @@
 import math
 from tkinter import *
 
-# ---------------------------- CONSTANTS ------------------------------- #
+# CONSTANTS
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
@@ -10,11 +10,12 @@ FONT_NAME = "Courier"
 WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+SECONDS_IN_MINUTE = 60
 reps = 0
 timer = None
 
 
-# ---------------------------- TIMER RESET ------------------------------- #
+# TIMER RESET
 
 def reset_timer():
     window.after_cancel(timer)
@@ -25,15 +26,15 @@ def reset_timer():
     reps = 0
 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# TIMER MECHANISM
 
 def start_timer():
     global reps
     reps += 1
 
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
+    work_sec = WORK_MIN * SECONDS_IN_MINUTE
+    short_break_sec = SHORT_BREAK_MIN * SECONDS_IN_MINUTE
+    long_break_sec = LONG_BREAK_MIN * SECONDS_IN_MINUTE
 
     if reps % 8 == 0:
         count_down(long_break_sec)
@@ -46,14 +47,11 @@ def start_timer():
         title_label.config(text="Work", fg=GREEN)
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+# COUNTDOWN MECHANISM
 def count_down(count):
-    count_min = math.floor(count / 60)
+    count_min = count // 60
     count_sec = count % 60
-    if count_sec < 10:
-        count_sec = f"0{count_sec}"
-
-    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec:02d}")
     if count > 0:
         global timer
         timer = window.after(1000, count_down, count - 1)
@@ -66,7 +64,7 @@ def count_down(count):
         check_marks.config(text=marks)
 
 
-# ---------------------------- UI SETUP ------------------------------- #
+# UI SETUP
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
