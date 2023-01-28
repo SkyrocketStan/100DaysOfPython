1f680b30a600a29c371676ccc816963eb3187095
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-28 13:11:40 +0300

    Day 28: pomodoro UI

--- Day 028/main.py
+++ Day 028/main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -10,8 +12,32 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------ #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ------------------------- COUNTDOWN MECHANISM ----------------------------- #
 
-# ---------------------------- UI SETUP ------------------------------- #
+# UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50), bg=YELLOW)
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato = PhotoImage(file="./tomato.png")
+canvas.create_image(100, 112, image=tomato)
+canvas.create_text(100, 140, text="00:00", fill="white", font=(FONT_NAME, 35,
+                                                               "bold"))
+canvas.grid(column=1, row=1)
+
+stat_button = Button(text="Start", highlightthickness=0)
+stat_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_mark = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_mark.grid(column=1, row=3)
+
+window.mainloop()
