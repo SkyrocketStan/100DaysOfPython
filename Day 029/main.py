ab5049d6bd50cbe40cf24cd06a0b96f06dd8b0bc
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-29 13:00:23 +0300

    Day 29: password manager UI (broken in linux)

--- Day 029/main.py
+++ Day 029/main.py
@@ -6,11 +6,33 @@ from tkinter import *
 # UI SETUP
 window = Tk()
 window.title("Password manager")
-window.config(pady=20, padx=20)
+window.config(pady=50, padx=50)
 
 canvas = Canvas(height=200, width=200)
 logo = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
