de0944a2fb8be1fa22556c428ed173b16fa08322
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-29 12:40:56 +0300

    Day 29: password manager logo

--- Day 029/main.py
+++ Day 029/main.py
@@ -1,5 +1,16 @@
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# PASSWORD GENERATOR
+from tkinter import *
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+# SAVE PASSWORD
 
-# ---------------------------- UI SETUP ------------------------------- #
+# UI SETUP
+window = Tk()
+window.title("Password manager")
+window.config(pady=20, padx=20)
+
+canvas = Canvas(height=200, width=200)
+logo = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo)
+canvas.pack()
+
+window.mainloop()
