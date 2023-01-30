43978f21e327f8f2a1a0536825cb336dad6a57a4
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-30 14:17:47 +0300

    Day 23: Exception Handling

---
+++ Day 030/errors_handling.py
@@ -0,0 +1,27 @@
+# Exception Handling
+
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    raise TypeError("This is an error that I made up.")
+
+# BMI Example
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
