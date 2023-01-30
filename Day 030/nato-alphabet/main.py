309b5c55f3567522c2758e86add668b71f9973f9
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-30 16:55:15 +0300

    Day 30 / Exception Handling in the NATO Phonetic Alphabet Project

---
+++ Day 030/nato-alphabet/main.py
@@ -0,0 +1,16 @@
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+while True:
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        break
+
+print(output_list)
