89ee4ba58b40e39be9e431098bc40577835892fd
Stanislav Rakitov <prosperousrf@gmail.com>
2023-01-30 20:23:25 +0300

    Day 30: Password generator. Refactor

--- Day 030/pass_gen/main.py
+++ Day 030/pass_gen/main.py
@@ -4,6 +4,8 @@ from tkinter import messagebox
 
 import password_generator
 
+JSON_FILE = "password.json"
+
 
 # PASSWORD GENERATOR
 def generate_password():
@@ -14,6 +16,20 @@ def generate_password():
     window.clipboard_append(password)
 
 
+def get_json_data() -> json:
+    try:
+        with open(JSON_FILE) as file:
+            data: json = json.load(file)
+    except FileNotFoundError:
+        data = None
+    return data
+
+
+def write_json_file(data):
+    with open(JSON_FILE, mode="w") as file:
+        json.dump(data, file, indent=4)
+
+
 # SAVE PASSWORD
 def save_password():
     website = website_entry.get()
@@ -21,6 +37,7 @@ def save_password():
     password = password_entry.get()
     new_data = {website: {"email": email, "password": password, }}
 
+    # check for empty fields
     if not website or not email or not password:
         messagebox.showwarning(message="Do not leave empty fields")
         return
@@ -31,19 +48,33 @@ def save_password():
                                            f"Email: {email}\n"
                                            f"Password: {password}")
     if is_ok:
-        try:
-            with open("password.json", mode="r") as file:
-                data = json.load(file)
+        data: json = get_json_data()
+        # Check for null data
+        if data is None:
+            data = new_data
 
-        except FileNotFoundError:
-            with open("password.json", mode="w") as file:
-                json.dump(new_data, file, indent=4)
-        else:
-            data.update(new_data)
+        data.update(new_data)
+        write_json_file(data)
 
         # clear fields in form
         website_entry.delete(0, END)
         password_entry.delete(0, END)
+
+
+def search_password():
+    website = website_entry.get()
+    data = get_json_data()
+
+    # Null check
+    if data is None or website not in data:
+        messagebox.showerror(title="Search result",
+                             message=f"Data not found.")
+        return
+
+    email = data[website]["email"]
+    password = data[website]["password"]
+    messagebox.showinfo(title=website,
+                        message=f"Email: {email}\nPassword: {password}")
 
 
 # UI SETUP
@@ -65,11 +96,11 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
-email_entry = Entry(width=35)
+email_entry = Entry(width=40)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "abuse@udemy.com")
 
@@ -82,5 +113,8 @@ generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save_password)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text="Search", width=15, command=search_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
