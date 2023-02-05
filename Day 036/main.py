8bafa2c4709e587fd5f93bdfc2106cb64670b74b
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-05 18:31:10 +0300

    Day 36: Stock news.

--- Day 036/main.py
+++ Day 036/main.py
@@ -2,6 +2,7 @@ import os
 
 import requests
 from dotenv import load_dotenv
+from twilio.rest import Client
 
 load_dotenv()  # This reads the environment variables inside .env
 
@@ -10,6 +11,11 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 STOCK_TOKEN = os.getenv("ALPHAVANTAGE")
+NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+TWILIO_SID = os.getenv("TWILIO_SID")
+TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN")
+VIRTUAL_TWILIO_NUMBER = os.getenv("VIRTUAL_TWILIO_NUMBER")
+VERIFIED_NUMBER = os.getenv("VERIFIED_NUMBER")
 
 
 def get_stock_percentage() -> float:
@@ -27,37 +33,59 @@ def get_stock_percentage() -> float:
     return stock_difference_percentage
 
 
-if get_stock_percentage() > -1:
+if get_stock_percentage() > 5:
     print("Get News")
 
 # STEP 2: https://newsapi.org/ Instead of printing ("Get News"), actually get
 # the first 3 news pieces for the COMPANY_NAME.
 
-# TODO 6. - Instead of printing ("Get News"), use the News API to get
-#  articles related to the COMPANY_NAME.
-
-# TODO 7. - Use Python slice operator to create a list that contains the
-#  first 3 articles. Hint: https://stackoverflow.com/questions/509211
-#  /understanding-slice-notation
+# I STUCK HERE! I Can't get the API
+# You didn't pass the captcha. Please try again.
+# So below is the code from end of lesson
 
 
-# STEP 3: Use twilio.com/docs/sms/quickstart/python to send a separate
-# message with each article's title and description to your phone number.
+up_down = None
+diff_percent = get_stock_percentage()
 
-# TODO 8. - Create a new list of the first 3 article's headline and
-#  description using list comprehension.
+# # STEP 2: Instead of printing ("Get News"), actually get the first 3 news
+# pieces for the COMPANY_NAME.
 
-# TODO 9. - Send each article as a separate message via Twilio.
+# Instead of printing ("Get News"), use the News API to get articles related
+# to the COMPANY_NAME. If difference percentage is greater than 5 then print(
+# "Get News").
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
 
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
 
-# Optional TODO: Format the message like this:
-"""TSLA: 🔺2% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (
-TSLA)?. Brief: We at Insider Monkey have gone over 821 13F filings that hedge 
-funds and prominent investors are required to file by the SEC The 13F filings 
-show the funds' and investors' portfolio positions as of March 31st, near the 
-height of the coronavirus market crash. or "TSLA: 🔻5% Headline: Were Hedge 
-Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey 
-have gone over 821 13F filings that hedge funds and prominent investors are 
-required to file by the SEC The 13F filings show the funds' and investors' 
-portfolio positions as of March 31st, near the height of the coronavirus 
-market crash."""
+    # Use Python slice operator to create a list that contains the first 3
+    # articles. Hint: https://stackoverflow.com/questions/509211
+    # /understanding-slice-notation
+    three_articles = articles[:3]
+    print(three_articles)
+
+    # # STEP 3: Use Twilio to send a seperate message with each article's
+    # title and description to your phone number.
+
+    # Create a new list of the first 3 article's headline and description
+    # using list comprehension.
+    formatted_articles = [
+        f"{STOCK_NAME}: {up_down}{diff_percent}%\n"
+        f"Headline: {article['title']}. \n"
+        f"Brief: {article['description']}"
+        for article in three_articles]
+    print(formatted_articles)
+    # Send each article as a separate message via Twilio.
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    # Send each article as a separate message via Twilio.
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
