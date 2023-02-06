bc30dda6f30c4b5f1660a836977f532430511bdd
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-06 17:15:56 +0300

    Day 37: Pixela add user

--- Day 037/main.py
+++ Day 037/main.py
@@ -1,17 +1,19 @@
 import requests
 
 PIXELA_API_TOKEN = "wVcvGqkpXFNvHK5o7aCtsUHpD"
+HEADERS_TOKEN = {"X-USER-TOKEN": PIXELA_API_TOKEN}
 PIXELA_API_URL = "https://pixe.la/v1/users"
-PIXELA_API_USERNAME = "anonymous101284"
-post_params = {"token": PIXELA_API_TOKEN,
-               "username": PIXELA_API_USERNAME,
-               "agreeTermsOfService": "yes",
-               "notMinor": "yes"}
-response = requests.post(PIXELA_API_URL, json=post_params)
-print(response.text)
-#
-#
-# delete_params = {"X-USER-TOKEN": PIXELA_API_TOKEN}
-# delete_url = PIXELA_API_URL + "/" + PIXELA_API_USERNAME
-# response = requests.delete(delete_url, json=delete_params)
-# print(response.text)
+USERNAME = "anonymous101284"
+HEADERS = {"X-USER-TOKEN": PIXELA_API_TOKEN}
+
+
+def make_user():
+    post_params = {"token": PIXELA_API_TOKEN,
+                   "username": USERNAME,
+                   "agreeTermsOfService": "yes",
+                   "notMinor": "yes"}
+    response = requests.post(PIXELA_API_URL, json=post_params)
+    print(response.text)
+
+
+make_user()
