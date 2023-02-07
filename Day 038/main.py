5274da1d1b493229e3b5eeb4f9e8d7cf6687d611
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-07 16:28:28 +0300

    Day 38: Tracker. Authentication

--- Day 038/main.py
+++ Day 038/main.py
@@ -46,7 +46,29 @@ for exercise in result["exercises"]:
             "calories": exercise["nf_calories"]
         }
     }
-
+    # No Authentication
     sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
 
     print(sheet_response.text)
+
+"""
+# Basic Authentication
+sheet_response = requests.post(
+    sheet_endpoint,
+    json=sheet_inputs,
+    auth=(
+        YOUR USERNAME,
+    YOUR PASSWORD,
+    )
+)
+
+# Bearer Token Authentication
+bearer_headers = {
+    "Authorization": "Bearer YOUR_TOKEN"
+}
+sheet_response = requests.post(
+    sheet_endpoint,
+    json=sheet_inputs,
+    headers=bearer_headers
+)
+"""
