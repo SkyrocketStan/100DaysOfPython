b866a9aa38138ea428b4690108c0d34a05b7a6be
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-07 16:25:55 +0300

    Day 38: Tracker. Saving Data into Google Sheets

--- Day 038/main.py
+++ Day 038/main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import requests
 from dotenv import load_dotenv
@@ -14,7 +15,7 @@ APP_ID = os.getenv("APP_ID")
 API_KEY = os.getenv("API_KEY")
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
-
+sheet_endpoint = "https://api.sheety.co/username/projectName/sheetName"
 exercise_text = input("Tell me which exercises you did: ")
 
 headers = {
@@ -32,4 +33,20 @@ parameters = {
 
 response = requests.post(exercise_endpoint, json=parameters, headers=headers)
 result = response.json()
-print(result)
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+
+    print(sheet_response.text)
