3bd2a9be359364d44faf5442aebc0998f79a80eb
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-09 11:44:34 +0300

    Day 40: flight_search

--- Day 040/flight_search.py
+++ Day 040/flight_search.py
@@ -1,11 +1,10 @@
+import os
+from pprint import pprint
+
 import requests
 from flight_data import FlightData
 
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = YOUR
-TEQUILA
-API
-KEY
 
 
 class FlightSearch:
@@ -16,7 +15,7 @@ class FlightSearch:
     def get_destination_codes(self, city_names):
         print("get destination codes triggered")
         location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
-        headers = {"apikey": TEQUILA_API_KEY}
+        headers = {"apikey": os.environ["TEQUILA_API_KEY"]}
         for city in city_names:
             query = {"term": city, "location_types": "city"}
             response = requests.get(url=location_endpoint, headers=headers,
@@ -30,7 +29,7 @@ class FlightSearch:
     def check_flights(self, origin_city_code, destination_city_code, from_time,
                       to_time):
         print(f"Check flights triggered for {destination_city_code}")
-        headers = {"apikey": TEQUILA_API_KEY}
+        headers = {"apikey": os.environ["TEQUILA_API_KEY"]}
         query = {
             "fly_from": origin_city_code,
             "fly_to": destination_city_code,
@@ -50,13 +49,32 @@ class FlightSearch:
             params=query,
         )
 
-        ################
         try:
             data = response.json()["data"][0]
-            print(f"{destination_city_code} {data['price']}")
         except IndexError:
-            print(f"No flights found for {destination_city_code}.")
-            return None
+
+            ##########################
+            query["max_stopovers"] = 1
+            response = requests.get(
+                url=f"{TEQUILA_ENDPOINT}/v2/search",
+                headers=headers,
+                params=query,
+            )
+            data = response.json()["data"][0]
+            pprint(data)
+            flight_data = FlightData(
+                price=data["price"],
+                origin_city=data["route"][0]["cityFrom"],
+                origin_airport=data["route"][0]["flyFrom"],
+                destination_city=data["route"][1]["cityTo"],
+                destination_airport=data["route"][1]["flyTo"],
+                out_date=data["route"][0]["local_departure"].split("T")[0],
+                return_date=data["route"][2]["local_departure"].split("T")[0],
+                stop_overs=1,
+                via_city=data["route"][0]["cityTo"]
+            )
+            return flight_data
+            ###########################
         else:
             flight_data = FlightData(
                 price=data["price"],
@@ -67,5 +85,5 @@ class FlightSearch:
                 out_date=data["route"][0]["local_departure"].split("T")[0],
                 return_date=data["route"][1]["local_departure"].split("T")[0]
             )
+
             return flight_data
-        ################
