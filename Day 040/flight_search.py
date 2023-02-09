cd7c7f3e0f34762307c9dd0b1704d082c36aaf32
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-09 11:41:32 +0300

    Day 40: Flight search

---
+++ Day 040/flight_search.py
@@ -0,0 +1,71 @@
+import requests
+from flight_data import FlightData
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = YOUR
+TEQUILA
+API
+KEY
+
+
+class FlightSearch:
+
+    def __init__(self):
+        self.city_codes = []
+
+    def get_destination_codes(self, city_names):
+        print("get destination codes triggered")
+        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
+        headers = {"apikey": TEQUILA_API_KEY}
+        for city in city_names:
+            query = {"term": city, "location_types": "city"}
+            response = requests.get(url=location_endpoint, headers=headers,
+                                    params=query)
+            results = response.json()["locations"]
+            code = results[0]["code"]
+            self.city_codes.append(code)
+
+        return self.city_codes
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time,
+                      to_time):
+        print(f"Check flights triggered for {destination_city_code}")
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 30,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
+
+        response = requests.get(
+            url=f"{TEQUILA_ENDPOINT}/v2/search",
+            headers=headers,
+            params=query,
+        )
+
+        ################
+        try:
+            data = response.json()["data"][0]
+            print(f"{destination_city_code} {data['price']}")
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+        else:
+            flight_data = FlightData(
+                price=data["price"],
+                origin_city=data["route"][0]["cityFrom"],
+                origin_airport=data["route"][0]["flyFrom"],
+                destination_city=data["route"][0]["cityTo"],
+                destination_airport=data["route"][0]["flyTo"],
+                out_date=data["route"][0]["local_departure"].split("T")[0],
+                return_date=data["route"][1]["local_departure"].split("T")[0]
+            )
+            return flight_data
+        ################
