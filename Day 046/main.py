db2d711c3e213b686065999e1e6014d0e45c6b7b
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-15 16:46:41 +0300

    Day 46: Billboard top 100. Step 3

--- Day 046/main.py
+++ Day 046/main.py
@@ -27,3 +27,19 @@ sp = spotipy.Spotify(
         cache_path="token.txt"
     )
 )
+
+user_id = sp.current_user()["id"]
+date = input(
+    "Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_names = ["The list of song", "titles from your", "web scrape"]
+
+song_uris = []
+year = date.split("-")[0]
+for song in song_names:
+    result = sp.search(q=f"track:{song} year:{year}", type="track")
+    print(result)
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped.")
