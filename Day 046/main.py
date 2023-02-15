3ea0c858e1690c1c8aa6ecd5bacb8e6dd5c5e061
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-15 16:49:40 +0300

    Day 46: Billboard top 100. Step 4

--- Day 046/main.py
+++ Day 046/main.py
@@ -3,36 +3,29 @@ import spotipy
 from bs4 import BeautifulSoup
 from spotipy.oauth2 import SpotifyOAuth
 
-date = input(
-    "Which year do you want to travel to? Type the date in this format "
-    "YYYY-MM-DD: ")
+# Scraping Billboard 100
+date = input("Which year do you want to travel to? Type the date in this "
+             "format YYYY-MM-DD: ")
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
-response.raise_for_status()
 soup = BeautifulSoup(response.text, 'html.parser')
-song_names_raw = soup.find_all("h3", class_="a-no-trucate")
-song_names = [song.getText().strip() for song in song_names_raw]
-print(song_names)
+song_names_spans = soup.find_all("span",
+                                 class_="chart-element__information__song")
+song_names = [song.getText() for song in song_names_spans]
 
-SPOTIPY_CLIENT_ID = "YOUR-SPOTIPY_CLIENT_ID"
-SPOTIPY_CLIENT_SECRET = "YOUR-SPOTIPY_CLIENT_SECRET"
-SPOTIPY_REDIRECT_URI = "http://localhost:8888/callback"  # here you need to put the same URI in your Spotify account
-
+# Spotify Authentication
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
-        scope="user-library-read playlist-modify-private",
-        redirect_uri=SPOTIPY_REDIRECT_URI,
-        client_id=SPOTIPY_CLIENT_ID,
-        client_secret=SPOTIPY_CLIENT_SECRET,
-        show_dialog=False,
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id="YOUR CLIENT ID", client_secret="YOUR CLIENT SECRET",
+        show_dialog=True,
         cache_path="token.txt"
     )
 )
-
 user_id = sp.current_user()["id"]
-date = input(
-    "Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-song_names = ["The list of song", "titles from your", "web scrape"]
+print(user_id)
 
+# Searching Spotify for songs by title
 song_uris = []
 year = date.split("-")[0]
 for song in song_names:
@@ -43,3 +36,11 @@ for song in song_names:
         song_uris.append(uri)
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
+
+# Creating a new private playlist in Spotify
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100",
+                                   public=False)
+print(playlist)
+
+# Adding songs found into the new playlist
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
