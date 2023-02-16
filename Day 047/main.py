4e3dccccee8a456c9d35bd13d4787d2bc9663989
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-16 13:05:11 +0300

    Day 47: Step 2 - Email Alert When Price Below Preset Value

--- Day 047/main.py
+++ Day 047/main.py
@@ -1,3 +1,5 @@
+import smtplib
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,6 +7,10 @@ URL = "https://www.amazon.com/NESCO-NPC-9-Pressure-Canner-Stainless/dp/B07VL6LX7
 UA = "MMozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5)" \
      "AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15"
 AL = "en-US"
+BUY_PRICE = 200
+SMTP_ADDRESS = "smtp"
+EMAIL = "noob@no.pe"
+PASSWORD = "password"
 
 
 def get_html_data():
@@ -18,4 +24,24 @@ data = get_html_data()
 soup = BeautifulSoup(data, "lxml")
 price = soup.find(name="span", class_="a-offscreen") \
     .getText().strip().replace("$", "")
-print(price)
+title = soup.find(id="productTitle").get_text().strip()
+
+
+# print(title)
+# print(price)
+
+def sent_email():
+    with smtplib.SMTP(SMTP_ADDRESS, port=587) as connection:
+        connection.starttls()
+        result = connection.login(EMAIL, PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL,
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{URL}"
+        )
+        print(result)
+
+
+if float(price) < BUY_PRICE:
+    message = f"{title} is now {price}"
+    sent_email()
