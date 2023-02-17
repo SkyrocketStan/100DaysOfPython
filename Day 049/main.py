d779f2c3dd46c8d78bed10c8fe2b59fc76793137
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-17 21:04:21 +0300

    Day 48: selenium cookie clicker

--- Day 049/main.py
+++ Day 049/main.py
@@ -1,5 +1,31 @@
+from time import time
+
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
-CHROMEDRIVER = "~/workspace/chromedriver"
+timeout = time() + 5
+five_min = time() + 60 * 5
+chrome_driver_path = "~/ws/chromedriver"
+service = Service(chrome_driver_path)
 
-driver = webdriver.Chrome(CHROMEDRIVER)
+driver = webdriver.Chrome(service=service)
+URL = "http://orteil.dashnet.org/experiments/cookie/"
+driver.get(URL)
+
+cookie = driver.find_element(by=By.ID, value="cookie")
+
+while True:
+    cookie.click()
+    if time() > timeout:
+        to_buy = []
+        shopping_cart = driver.find_elements(by=By.CSS_SELECTOR,
+                                             value="#store div")
+        for item in shopping_cart[:-1]:
+            if item.get_attribute("class") != "grayed":
+                to_buy.append(item)
+        to_buy[-1].click()
+        timeout = time() + 5
+        if time() > five_min:
+            print(driver.find_element(By.ID, value="cps").text)
+            break
