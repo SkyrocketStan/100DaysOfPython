0a0191117fd308186c95864adea70310f437c49c
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-19 11:58:33 +0300

    Day 50: Step 4 - Dismiss all requests

---
+++ Day 050/D50-Step4.md
@@ -0,0 +1,23 @@
+# Step 4 - Dismiss all requests
+When you first login to Tinder, it will ask if it's ok to get your location, send you notifications and track your cookies. We need to dismiss all of these modal pop-ups.
+
+Location Pop-up:
+![](https://img-b.udemycdn.com/redactor/raw/2020-08-21_15-43-00-49d934f6791bf4bad82b5438baa025e3.png)
+
+Notification Pop-up:
+![](https://img-b.udemycdn.com/redactor/raw/2020-08-21_15-43-16-4cee84dbd662e94e2d76238197f7b572.png)
+
+Cookies Pop-up:
+![](https://img-b.udemycdn.com/redactor/raw/2020-08-21_15-43-45-70a2a3812ac11797f8767a6529da9aa8.png)
+
+1. Using Selenium and Python:
+
+- Click ALLOW for location.
+
+- Click NOT INTERESTED for notifications.
+
+- Click I ACCEPT for cookies
+
+HINT 1: Finding the XPath will make it easier to target each element.
+
+HITN 2: Adding some delay before targeting these elements will allow time for them to load up.
--- Day 050/main.py
+++ Day 050/main.py
@@ -18,20 +18,33 @@ sleep(2)
 fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
 fb_login.click()
 
-#Switch to Facebook login window
 sleep(2)
 base_window = driver.window_handles[0]
 fb_login_window = driver.window_handles[1]
 driver.switch_to.window(fb_login_window)
 print(driver.title)
 
-#Login and hit enter
 email = driver.find_element_by_xpath('//*[@id="email"]')
 password = driver.find_element_by_xpath('//*[@id="pass"]')
+
 email.send_keys(FB_EMAIL)
 password.send_keys(FB_PASSWORD)
 password.send_keys(Keys.ENTER)
 
-#Switch back to Tinder window
 driver.switch_to.window(base_window)
 print(driver.title)
+
+#Delay by 5 seconds to allow page to load.
+sleep(5)
+
+#Allow location
+allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+allow_location_button.click()
+
+#Disallow notifications
+notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+
+#Allow cookies
+cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
