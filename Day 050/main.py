bc5a6a55b5b0df7264f30328999fc3c1eaafe43c
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-19 11:55:54 +0300

    Day 50: Step 3 - Login with Facebook

--- Day 050/main.py
+++ Day 050/main.py
@@ -1,20 +1,37 @@
-import os
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+FB_EMAIL = YOUR FACEBOOK LOGIN EMAIL
+FB_PASSWORD = YOUR FACEBOOK PASSWORD
 
-FB_EMAIL = os.getenv("app.email")
-FB_PASSWORD = os.getenv("app.pass")
-
-driver = webdriver.Chrome()
+chrome_driver_path = YOUR CHROME DRIVER PATH
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
 
 driver.get("http://www.tinder.com")
 
 sleep(2)
-login_button = driver.find_element(by=By.XPATH, value='//*[@id="c-1351236777"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a/div[2]/div[2]')
+login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
 login_button.click()
 
 sleep(2)
-fb_login = driver.find_element(by=By.XPATH, value='//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
 fb_login.click()
+
+#Switch to Facebook login window
+sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+#Login and hit enter
+email = driver.find_element_by_xpath('//*[@id="email"]')
+password = driver.find_element_by_xpath('//*[@id="pass"]')
+email.send_keys(FB_EMAIL)
+password.send_keys(FB_PASSWORD)
+password.send_keys(Keys.ENTER)
+
+#Switch back to Tinder window
+driver.switch_to.window(base_window)
+print(driver.title)
