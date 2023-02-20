5bfdba256c19f11888ff948d76c0ab56af84fd27
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-20 15:09:49 +0300

    Day 51: Step 3 - Get Internet Speeds

---
+++ Day 051/D51-Step3.md
@@ -0,0 +1,17 @@
+Step 3 - Get Internet Speeds
+
+1. Use [this speedtest website](https://www.speedtest.net/) to get your current
+   live download and upload speeds manually. e.g.
+   ![](https://img-c.udemycdn.com/redactor/raw/2020-08-24_10-49-41-51e5d6636b152e0be19783a8e5ff65ca.png)
+
+2. Use Selenium and Python to get the same result printed out in your console.
+   e.g.
+   ![](https://img-c.udemycdn.com/redactor/raw/2020-08-24_10-50-54-d7ba97ec7f028e293e22fb2471f015b8.png)
+
+HINT 1: First use Selenium to get the speedtest address, then see which steps
+you have to make your bot go through to get those results.
+
+HINT 2: Depending on your internet speeds, you might need to add a 60-180s delay
+to wait for the results.
+
+HINT 3: Consider using XPATH, if you can't figure out how to target the element.
--- Day 051/main.py
+++ Day 051/main.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 
 PROMISED_DOWN = 150
@@ -23,3 +25,21 @@ class InternetSpeedTwitterBot:
 bot = InternetSpeedTwitterBot(CHROME_DRIVER_PATH)
 bot.get_internet_speed()
 bot.tweet_at_provider()
+
+
+def get_internet_speed(self):
+    self.driver.get("https://www.speedtest.net/")
+
+    # Depending on your location, you might need to accept the GDPR pop-up.
+    # accept_button = self.driver.find_element_by_id("_evidon-banner-acceptbutton")
+    # accept_button.click()
+
+    time.sleep(3)
+    go_button = self.driver.find_element_by_css_selector(".start-button a")
+    go_button.click()
+
+    time.sleep(60)
+    self.up = self.driver.find_element_by_xpath(
+        '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+    self.down = self.driver.find_element_by_xpath(
+        '//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
