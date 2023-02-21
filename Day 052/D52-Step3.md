b5071755ca6ea134b69330c5cee89e9111b466dc
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-21 15:06:33 +0300

    Day 52: Step 3 - Login to Instagram

---
+++ Day 052/D52-Step3.md
@@ -0,0 +1,7 @@
+# Step 3 - Login to Instagram
+
+1. Use Selenium and Python to login to Instagram automatically using your email
+   and password. Write your code in the login() method.
+
+Here is the URL: https://www.instagram.com/accounts/login/
+
--- Day 052/main.py
+++ Day 052/main.py
@@ -1,4 +1,7 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 CHROME_DRIVER_PATH = "YOUR CHROME DRIVER PATH"
 SIMILAR_ACCOUNT = "INSTAGRAM ACCOUNT YOU WANT TO BECOME"
@@ -12,7 +15,17 @@ class InstaFollower:
         self.driver = webdriver.Chrome(executable_path=path)
 
     def login(self):
-        pass
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        time.sleep(5)
+
+        username = self.driver.find_element_by_name("username")
+        password = self.driver.find_element_by_name("password")
+
+        username.send_keys(USERNAME)
+        password.send_keys(PASSWORD)
+
+        time.sleep(2)
+        password.send_keys(Keys.ENTER)
 
     def find_followers(self):
         pass
