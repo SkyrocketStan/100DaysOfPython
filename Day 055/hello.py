8db7561c680a51b8f5560c324ca8a0180c9d2de0
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-24 15:41:23 +0300

    Day 54: Flask. Variables

---
+++ Day 055/hello.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return ("Hello world")
+
+
+@app.route('/bye')
+def bye():
+    return ("Bye!")
+
+
+@app.route('/user/<username>')
+def greet(username):
+    return f"Hello {username}!"
+
+
+if __name__ == "__main__":
+    app.run()
