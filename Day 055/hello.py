f2b9e2f3d4f57ec8d28ebe283200f1b01d5a5078
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-24 21:06:38 +0300

    Day 55: Decorators

--- Day 055/hello.py
+++ Day 055/hello.py
@@ -3,14 +3,37 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+
+    return wrapper
+
+
 @app.route('/')
+@make_bold
+@make_underlined
 def hello_world():
-    return ("Hello world")
+    return "Hello world"
 
 
 @app.route('/bye')
 def bye():
-    return ("Bye!")
+    return "Bye!"
 
 
 @app.route('/user/<username>')
