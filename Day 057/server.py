3fc18a06330e41ba98fbedf7e8100c00f539da16
Stanislav Rakitov <prosperousrf@gmail.com>
2023-02-26 12:14:28 +0300

    Day 57: Flask server with variables

--- Day 057/server.py
+++ Day 057/server.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    current_year = datetime.datetime.now().year
+    return render_template("index.html", year=current_year)
 
 
 if __name__ == '__main__':
