3608a7f22f10779fddb386fa13077d3930bf9ded
Stanislav Rakitov <prosperousrf@gmail.com>
2023-03-01 13:24:39 +0300

    Day 60: post

--- Day 060/main.py
+++ Day 060/main.py
@@ -1,9 +1,13 @@
+import smtplib
+
 import requests
 from flask import Flask, render_template, request
 
 posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
-
+OWN_EMAIL = "YOUR OWN EMAIL ADDRESS"
+OWN_PASSWORD = "YOUR EMAIL ADDRESS PASSWORD"
 app = Flask(__name__)
+
 
 @app.route('/')
 def get_all_posts():
@@ -23,16 +27,24 @@ def show_post(index):
 def about():
     return render_template("about.html")
 
+
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
         data = request.form
-        print(data["name"])
-        print(data["email"])
-        print(data["phone"])
-        print(data["message"])
+        send_email(data["name"], data["email"], data["phone"], data["message"])
         return render_template("contact.html", msg_sent=True)
     return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\n" \
+                    f"Name: {name}\nEmail: {email}\n" \
+                    f"Phone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
