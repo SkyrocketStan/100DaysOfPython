f7705e6b1bcde534a02e960a8e94612ac34c8157
Stanislav Rakitov <prosperousrf@gmail.com>
2023-03-02 16:42:38 +0300

    Day 61: Flask-Bootstrap Supports WTForms

--- Day 061/main.py
+++ Day 061/main.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 from login_form import LoginForm
 
 app = Flask(__name__)
 app.secret_key = "any-string-you-want-just-keep-it-secret"
+Bootstrap(app)
 
 
 @app.route("/")
@@ -14,13 +16,13 @@ def home():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
-    if login_form.validate_on_submit():
-        if login_form.email.data == 'email@email.com' and \
-                login_form.password.data == "123456789":
-            return render_template("success.html")
-        else:
-            return render_template("denied.html")
-    return render_template("login.html", form=login_form)
+    if not login_form.validate_on_submit():
+        return render_template("login.html", form=login_form)
+
+    if login_form.email.data == 'email@email.com' and login_form.password.data == "123456789":
+        return render_template("success.html")
+
+    return render_template("denied.html")
 
 
 if __name__ == '__main__':
