e0b17173df30133c1e5d0d0ab9bdbeaf74160869
Stanislav Rakitov <prosperousrf@gmail.com>
2023-03-04 12:29:57 +0300

    Day 63: Creating a Virtual Bookshelf

--- Day 063/main.py
+++ Day 063/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,12 +7,25 @@ all_books = []
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(new_book)
+
+        # NOTE: You can use the redirect method from flask to redirect to
+        # another route e.g. in this case to the home page after the form has
+        # been submitted.
+        return redirect(url_for('home'))
+
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
