312137d803a1f76b29a5b7fe40e3aeb99207a52e
Stanislav Rakitov <prosperousrf@gmail.com>
2023-03-05 11:18:28 +0300

    Day 64:  Be Able to Add New Movies Via the Add Page

--- Day 064/main.py
+++ Day 064/main.py
@@ -66,6 +66,7 @@ def rate_movie():
         return redirect(url_for('home'))
     return render_template("edit.html", movie=movie, form=form)
 
+
 @app.route("/delete")
 def delete_movie():
     movie_id = request.args.get("id")
@@ -73,6 +74,50 @@ def delete_movie():
     db.session.delete(movie)
     db.session.commit()
     return redirect(url_for("home"))
+
+
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(MOVIE_DB_SEARCH_URL,
+                                params={"api_key": MOVIE_DB_API_KEY,
+                                        "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        # The language parameter is optional, if you were making the website for a different audience
+        # e.g. Hindi speakers then you might choose "hi-IN"
+        response = requests.get(movie_api_url,
+                                params={"api_key": MOVIE_DB_API_KEY,
+                                        "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            # The data in release_date includes month and day, we will want to get rid of.
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("home"))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
