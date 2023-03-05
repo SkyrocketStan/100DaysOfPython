98dbc10f3a9a0279331f6e674fe8b7074ca3d8f2
Stanislav Rakitov <prosperousrf@gmail.com>
2023-03-05 11:22:38 +0300

    Day 64:  Be Able to Sort and Rank the Movies By Rating

--- Day 064/main.py
+++ Day 064/main.py
@@ -1,6 +1,15 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+MOVIE_DB_API_KEY = "GET YOUR API KEY FROM themoviedb.org"
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -22,36 +31,61 @@ class Movie(db.Model):
     ranking = db.Column(db.Integer, nullable=True)
     review = db.Column(db.String(250), nullable=True)
     img_url = db.Column(db.String(250), nullable=False)
-
-
 db.create_all()
 
 
-## After adding the new_movie the code needs to be commented out/deleted.
-## So you are not trying to add the same movie twice.
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# db.session.add(new_movie)
-# db.session.commit()
-
-
-@app.route("/")
-def home():
-    all_movies = Movie.query.all()
-    return render_template("index.html", movies=all_movies)
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
 
 
 class RateMovieForm(FlaskForm):
     rating = StringField("Your Rating Out of 10 e.g. 7.5")
     review = StringField("Your Review")
     submit = SubmitField("Done")
+
+
+@app.route("/")
+def home():
+    all_movies = Movie.query.order_by(Movie.rating).all()
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+
+        response = requests.get(MOVIE_DB_SEARCH_URL,
+                                params={"api_key": MOVIE_DB_API_KEY,
+                                        "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url,
+                                params={"api_key": MOVIE_DB_API_KEY,
+                                        "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("rate_movie", id=new_movie.id))
 
 
 @app.route("/edit", methods=["GET", "POST"])
@@ -74,49 +108,6 @@ def delete_movie():
     db.session.delete(movie)
     db.session.commit()
     return redirect(url_for("home"))
-
-
-class FindMovieForm(FlaskForm):
-    title = StringField("Movie Title", validators=[DataRequired()])
-    submit = SubmitField("Add Movie")
-
-
-@app.route("/add", methods=["GET", "POST"])
-def add_movie():
-    form = FindMovieForm()
-
-    if form.validate_on_submit():
-        movie_title = form.title.data
-        response = requests.get(MOVIE_DB_SEARCH_URL,
-                                params={"api_key": MOVIE_DB_API_KEY,
-                                        "query": movie_title})
-        data = response.json()["results"]
-        return render_template("select.html", options=data)
-
-    return render_template("add.html", form=form)
-
-
-@app.route("/find")
-def find_movie():
-    movie_api_id = request.args.get("id")
-    if movie_api_id:
-        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
-        # The language parameter is optional, if you were making the website for a different audience
-        # e.g. Hindi speakers then you might choose "hi-IN"
-        response = requests.get(movie_api_url,
-                                params={"api_key": MOVIE_DB_API_KEY,
-                                        "language": "en-US"})
-        data = response.json()
-        new_movie = Movie(
-            title=data["title"],
-            # The data in release_date includes month and day, we will want to get rid of.
-            year=data["release_date"].split("-")[0],
-            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
-            description=data["overview"]
-        )
-        db.session.add(new_movie)
-        db.session.commit()
-        return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
