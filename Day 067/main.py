91e293714e6418370f45ceaca5881c86fd3be3d3
Stanislav Rakitov <prosperousrf@gmail.com>
2023-03-08 13:44:35 +0300

    Day 67: : make post

--- Day 067/main.py
+++ Day 067/main.py
@@ -57,6 +57,7 @@ def add_new_post():
         db.session.commit()
         return redirect(url_for("get_all_posts"))
 
+
 # RENDER HOME PAGE USING DB
 @app.route('/')
 def get_all_posts():
@@ -79,6 +80,27 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
 if __name__ == "__main__":
