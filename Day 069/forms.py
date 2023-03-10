8402bd08006bd875b792973533646dcbb4af143b
Stanislav Rakitov <prosperousrf@gmail.com>
2023-03-10 10:05:55 +0300

    Day 69: Start code

---
+++ Day 069/forms.py
@@ -0,0 +1,13 @@
+from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+
+
+##WTForm
+class CreatePostForm(FlaskForm):
+    title = StringField("Blog Post Title", validators=[DataRequired()])
+    subtitle = StringField("Subtitle", validators=[DataRequired()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit Post")
